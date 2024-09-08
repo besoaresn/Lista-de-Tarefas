fa50a39daaaa9d64ec1f1d8e65f8008901f98b84
Bê Soares <bernardosoaresdev@hotmail.com>
2024-09-07 22:50:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 function addTask() {
   const input = document.getElementById('taskInput');
   const taskText = input.value.trim();
+  const taskList = document.getElementById('taskList');
 
   if (taskText === '') {
-      alert('Digite uma tarefa!');
-      return;
+    alert('Digite uma tarefa!');
+    return;
   }
 
-  const taskList = document.getElementById('taskList');
+  if (taskList.children.length >= 5) {
+    alert('Você atingiu o limite máximo de 5 tarefas!');
+    return;
+  }
+
   const li = document.createElement('li');
   li.innerHTML = `
-      ${taskText}
-      <button onclick="removeTask(this)">Remover</button>
+    ${taskText}
+    <button onclick="removeTask(this)">Remover</button>
   `;
 
   taskList.appendChild(li);
